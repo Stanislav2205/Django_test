627b6abc838a878d9c971363ea33f3ffe6ffdea5
Stanislav_SPB <stanislav.usmaev@gmail.com>
2025-09-28 16:34:36 +0300

    Мой новый коммит

--- django_testing/django_testing/settings.py
+++ django_testing/django_testing/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_django_testing',
         'USER': 'postgres',
-        'PASSWORD': 'C616ov797',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
